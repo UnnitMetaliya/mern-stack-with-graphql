930c14437250a7e89d41b0031b517d49c71d547a
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-06-29 18:39:39 -0400

    created react router dom

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,12 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Edit from "./components/Edit";
+import Create from "./components/Create";
+import Show from "./components/Show";
+
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
@@ -13,6 +19,14 @@ ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
+  <Route>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/edit/:id" component={Edit} />
+      <Route path="/create" component={Create} />
+      <Route path="/show/:id" component={Show} />
+    </div>
+  </Route>,
   document.getElementById("root")
 );
 
